669b37d580c85491f36e380bfadbfce7618fe356
prucinski <p.rucinski.19@abdn.ac.uk>
2021-11-17 23:24:32 +0000

    Created a basic UI

---
+++ app/src/main/java/com/example/ocrhotel/CameraProcessor.kt
@@ -0,0 +1,8 @@
+package com.example.ocrhotel
+
+class CameraProcessor {
+    public fun takeImage(){
+        //just a skeleton, do whatever you want with it! Its here just for reference
+        //for UI (see MainMenu). Feel free to update that section too for testing ~Piotr
+    }
+}
--- app/src/main/java/com/example/ocrhotel/MainMenu.kt
+++ app/src/main/java/com/example/ocrhotel/MainMenu.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.ocrhotel.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainMenu : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -32,8 +33,28 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+
+
+        binding.scan.setOnClickListener {
+            val camera = CameraProcessor()
+            camera.takeImage()
+            //this navigation doesn't mean anything yet
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            //I have no idea how this is going to go here. We might call a custom method invokeCamera(),
+            //which is a separate class. That thing is then done and we go to a new screen with
+            //confirmation/rejection
+            //https://developer.android.com/training/camera/photobasics useful documentation
+        }
+        binding.upload.setOnClickListener {
+            //again - this section  needs expanding
+            val helloUpload = Toast.makeText(context, "This will lead to the gallery", Toast.LENGTH_SHORT)
+            helloUpload.show()
+
+        }
+        binding.tutorial.setOnClickListener {
+            //toast is kind of like printing to console, but in android - idk how to get this to work
+            val helloTutorial = Toast.makeText(context, "@string/tutorial_button_message", Toast.LENGTH_SHORT)
+            helloTutorial.show()
         }
     }
 
