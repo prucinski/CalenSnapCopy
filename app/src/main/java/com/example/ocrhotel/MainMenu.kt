39cae82c030dc676e4670a314a73b7fb8bbfce8d
erikps <erikps>
2021-11-22 02:54:05 +0000

    Added support for scanning for a poster as well as loading an image from disk.

--- app/src/main/java/com/example/ocrhotel/MainMenu.kt
+++ app/src/main/java/com/example/ocrhotel/MainMenu.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.ocrhotel.databinding.FragmentFirstBinding
+import java.io.File
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -30,30 +33,40 @@ class MainMenu : Fragment() {
 
     }
 
+    private val filePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            // TODO: handle return value
+        }
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            // TODO: handle return value
+        }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
         binding.scan.setOnClickListener {
-            val camera = CameraProcessor()
-            camera.takeImage()
-            //this navigation doesn't mean anything yet
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-            //I have no idea how this is going to go here. We might call a custom method invokeCamera(),
-            //which is a separate class. That thing is then done and we go to a new screen with
-            //confirmation/rejection
-            //https://developer.android.com/training/camera/photobasics useful documentation
+            // Create a temporary file for storing the picture about to be taken
+            val file = File(context?.filesDir, "tempPictureFile")
+            val a = activity
+            a?.let {
+                // Get uri for the file that was just created
+                val uri = FileProvider.getUriForFile(
+                    Objects.requireNonNull(a.applicationContext),
+                    BuildConfig.APPLICATION_ID + ".provider", file
+                );
+                // Actually launch the camera
+                cameraLauncher.launch(uri)
+            }
         }
         binding.upload.setOnClickListener {
-            //again - this section  needs expanding
-            val helloUpload = Toast.makeText(context, "This will lead to the gallery", Toast.LENGTH_SHORT)
-            helloUpload.show()
-
+            filePickerLauncher.launch("image/*")
         }
         binding.tutorial.setOnClickListener {
             //toast is kind of like printing to console, but in android - idk how to get this to work
-            val helloTutorial = Toast.makeText(context, "@string/tutorial_button_message", Toast.LENGTH_SHORT)
+            val helloTutorial =
+                Toast.makeText(context, "@string/tutorial_button_message", Toast.LENGTH_SHORT)
             helloTutorial.show()
         }
     }
