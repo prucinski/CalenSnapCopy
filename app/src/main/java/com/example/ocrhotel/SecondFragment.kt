14e02aae7feb3b261c7b7cf474edd287a8f540a1
Alexandra <55953609+AlexaN-00@users.noreply.github.com>
2021-11-22 15:47:29 +0000

    Update SecondFragment.kt

--- app/src/main/java/com/example/ocrhotel/SecondFragment.kt
+++ app/src/main/java/com/example/ocrhotel/SecondFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.navigation.fragment.findNavController
 import com.example.ocrhotel.databinding.FragmentSecondBinding
+import java.io.File
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -29,8 +34,39 @@ class SecondFragment : Fragment() {
 
     }
 
+    private val filePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            // TODO: handle return value
+        }
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            // TODO: handle return value
+        }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.uploadImage.setOnClickListener {
+            //again - this section  needs expanding
+            filePickerLauncher.launch("image/*")
+//            val helloUpload = Toast.makeText(context, "Now this will lead to the gallery", Toast.LENGTH_SHORT)
+//            helloUpload.show()
+        }
+
+        binding.camera.setOnClickListener {
+            val file = File(context?.filesDir, "tempPictureFile")
+            val a = activity
+            a?.let {
+                // Get uri for the file that was just created
+                val uri = FileProvider.getUriForFile(
+                    Objects.requireNonNull(a.applicationContext),
+                    BuildConfig.APPLICATION_ID + ".provider", file
+                );
+                // Actually launch the camera
+                cameraLauncher.launch(uri)
+            }
+        }
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
