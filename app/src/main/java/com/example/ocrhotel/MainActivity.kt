9725215d9e0a08c1853cd51d7ee562124ff169ad
prucinski <p.rucinski.19@abdn.ac.uk>
2021-11-26 23:38:59 +0000

    Removed toolbar & adjusted message button

--- app/src/main/java/com/example/ocrhotel/MainActivity.kt
+++ app/src/main/java/com/example/ocrhotel/MainActivity.kt
@@ -22,18 +22,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        //setSupportActionBar(binding.toolbar)
+
+
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
-
+//NOW I BELIEVE THAT THIS CAN BE THROWN OUT SINCE WE'RE NOT GENERATING A TOOLBAR
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
