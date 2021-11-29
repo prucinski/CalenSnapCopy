35d59ba533744d15f2aa71567372e7c15316ea91
erikps <erikps>
2021-11-29 13:16:10 +0000

    Added class 'EventCreator' with placeholder content.

---
+++ app/src/main/java/com/example/ocrhotel/EventCreator.kt
@@ -0,0 +1,19 @@
+package com.example.ocrhotel
+
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.AnalyzeResults
+import java.util.*
+
+class EventCreator {
+
+    class EventPlaceholder(public val dateMilliseconds: Long, public val name: String)
+
+    fun createEvent(results: AnalyzeResults): EventPlaceholder {
+        val date = Date().time
+        val name = "Placeholder"
+
+        val event = EventPlaceholder(date, name)
+
+        return event
+    }
+
+}
