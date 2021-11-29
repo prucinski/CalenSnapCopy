62cafc6a1be8905dfeccc44beb75de47d07ed4d8
erikps <erikps>
2021-11-29 15:47:10 +0000

    Added some more scaffolding code, main function for testing purposes. Can be run in Android Studio by clicking the little green play button.

--- app/src/main/java/com/example/ocrhotel/EventCreator.kt
+++ app/src/main/java/com/example/ocrhotel/EventCreator.kt
@@ -3,9 +3,27 @@ package com.example.ocrhotel
 import com.microsoft.azure.cognitiveservices.vision.computervision.models.AnalyzeResults
 import java.util.*
 
+const val exampleString =
+    "Organised by SPE International Aberdeen Section Well Decommisioning - The Future 20 April 2022, P&J Live SUBMIT YOUR ABSTRACT TODAY!"
+
+/**
+ *  TODO
+ *  - Handle american style dates, i.e. MM.DD.YYYY
+ *  - Handle multiple dates for the same event, e.g. 21-22 April 2022
+ */
+
+
+
 class EventCreator {
 
     class EventPlaceholder(public val dateMilliseconds: Long, public val name: String)
+
+    fun extractDates(text: String): MutableList<Long> {
+        var result= mutableListOf<Long>()
+
+
+        return result
+    }
 
     fun createEvent(rawText: String): EventPlaceholder {
 
@@ -18,4 +36,8 @@ class EventCreator {
         return event
     }
 
+}
+
+fun main() {
+    System.out.println("Hello World!")
 }
