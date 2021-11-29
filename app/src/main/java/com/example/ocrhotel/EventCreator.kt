215bc01c64c6b78132e9ed3a060cdef08cf4da02
erikps <erikps>
2021-11-29 17:06:21 +0000

    Dates can now be extracted as unix epoch timestamps. Times still need work

--- app/src/main/java/com/example/ocrhotel/EventCreator.kt
+++ app/src/main/java/com/example/ocrhotel/EventCreator.kt
@@ -1,5 +1,6 @@
 package com.example.ocrhotel
 
+import java.time.*
 import java.util.*
 
 const val exampleString =
@@ -12,47 +13,66 @@ const val exampleString =
  *  - Handle cases where year is omitted
  */
 
-val months = arrayOf(
-    "january",
-    "february",
-    "march",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "september",
-    "october",
-    "november",
-    "december"
+val months = hashMapOf(
+    "january" to 1,
+    "february" to 2,
+    "march" to 3,
+    "april" to 4,
+    "may" to 5,
+    "june" to 6,
+    "july" to 7,
+    "august" to 8,
+    "september" to 9,
+    "october" to 10,
+    "november" to 11,
+    "december" to 12,
+    "jan" to 1,
+    "feb" to 2,
+    "mar" to 3,
+    "apr" to 4,
+    "may" to 5,
+    "jun" to 6,
+    "jul" to 7,
+    "aug" to 8,
+    "sep" to 9,
+    "oct" to 10,
+    "nov" to 11,
+    "dec" to 12
 )
-val monthsShort =
-    arrayOf("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec")
 
 class EventCreator {
 
     class EventPlaceholder(public val dateMilliseconds: Long, public val name: String)
 
-    fun extractDates(text: String): MutableList<Long> {
-        var result = mutableListOf<Long>()
+    private val monthsString = months.keys.joinToString(separator = "|")
+    private val regex = Regex("""(\d\d)[/ .-]($monthsString|\d?\d)[/ .-]((?:\d\d)?\d\d|)""")
 
-        val monthsString = (months + monthsShort).joinToString(separator = "|")
-        val regex = Regex("""(\d\d)[/ .-]($monthsString|\d?\d)[/ .-]((?:\d\d)?\d\d|)""")
-
-        println(regex.pattern)
+    fun extractDates(text: String): Sequence<Long> {
         val matches = regex.findAll(text.lowercase())
-        for (match in matches) {
-            println("Day: ${match.groups[1]}")
-            println("Month: ${match.groups[2]}")
-            println("Year: ${match.groups[3]}")
+        val dates: Sequence<LocalDateTime> = matches.map { match ->
+            val day = match.groups[1]?.value?.toInt()
+            val month = match.groups[2]?.value?.let { m ->
+                var res = m.toIntOrNull()
+                if (res == null) {
+                    res = months.get(m)
+                }
+                res
+            }
+            val year = match.groups[3]?.value?.toInt()
+            if (year == null || month == null || day == null) {
+            } else {
+                // TODO: Handle times
+                return@map LocalDateTime.of(year, month, day, 0, 0, 0)
+            }
+            return@map LocalDateTime.now()
+        }
+        return dates.map { it ->
+            it.atZone(ZoneId.systemDefault()).toEpochSecond()
         }
 
-        return result
     }
 
     fun createEvent(rawText: String): EventPlaceholder {
-
-
         val date = Date().time
         val name = "Placeholder"
 
@@ -66,5 +86,7 @@ class EventCreator {
 fun main() {
     val creator = EventCreator()
 
-    creator.extractDates(exampleString)
+    for (date in creator.extractDates(exampleString)) {
+        println(Instant.ofEpochSecond(date))
+    }
 }
