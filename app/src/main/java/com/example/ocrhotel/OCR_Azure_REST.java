d9b5c4b2e1ba2b39c548eae41a5b870b71e02944
Matey <matey_krastev2@hotmail.com>
2021-11-30 12:57:25 +0200

    OCR API asynchronous, but cannot wait for values to return.

--- app/src/main/java/com/example/ocrhotel/OCR_Azure_REST.java
+++ app/src/main/java/com/example/ocrhotel/OCR_Azure_REST.java
@@ -1,6 +1,12 @@
 package com.example.ocrhotel;
 
+
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresPermission;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.microsoft.azure.cognitiveservices.vision.computervision.*;
 import com.microsoft.azure.cognitiveservices.vision.computervision.implementation.ComputerVisionImpl;
 import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
@@ -8,11 +14,15 @@ import java.io.File;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 import java.util.UUID;
 
 
@@ -26,25 +36,29 @@ public class OCR_Azure_REST {
     private static final String endpoint = "https://ocr-app.cognitiveservices.azure.com";
 
     private static final String POST_URLBase = endpoint + "/vision/v3.2/read/analyze";
-    // private static final String GET_URLBase = endpoint + "/vision/v3.2/read/analyzeResults/";
+    private static final String GET_URLBase = endpoint + "/vision/v3.2/read/analyzeResults/";
 
     private static final OkHttpClient client = new OkHttpClient();
     private static final MediaType MEDIA_TYPE_FILE = MediaType.parse("application/octet-stream");
     private static final MediaType MEDIA_TYPE_URL = MediaType.parse("application/json");
     private static final ComputerVisionClient compVisClient = ComputerVisionManager.authenticate(subscriptionKey).withEndpoint(endpoint);
 
+    private ReadOperationResult results = null;
+    private String resultsText;
+
     /**
      * Processes raw file data.
+     *
      * @param file File to be received
      * @return results as a ReadOperationResult to format later
-     * */
-    public ReadOperationResult GetImageTextData(File file) throws Exception{
+     */
+    public ReadOperationResult GetImageTextData(File file) throws Exception {
 
         Request request = new Request.Builder()
                 .addHeader("Content-Type", "application/octet-stream")
                 .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
                 .url(POST_URLBase)
-                .post(RequestBody.create(file,MEDIA_TYPE_FILE))
+                .post(RequestBody.create(file, MEDIA_TYPE_FILE))
                 .build();
 
         Response response = client.newCall(request).execute();
@@ -53,8 +67,8 @@ public class OCR_Azure_REST {
 
             System.out.println(response.headers().toString());
             System.out.println(response.body().string());
+        } catch (IOException e) {
         }
-        catch(IOException e){}
 
         String operationId = extractOperationIdFromOpLocation(response.headers().get("operation-location"));
         ComputerVisionImpl vision = (ComputerVisionImpl) compVisClient.computerVision();
@@ -62,34 +76,144 @@ public class OCR_Azure_REST {
         return GetReadResult(vision, operationId);
     }
 
-    /**
-     * Processes raw file data from URL.
-     * @param url URL of the file to be received
-     * @return results as a ReadOperationResult to format later
-     * Note: It is only here for testing purposes, don't mind it.
-     * */
-    public ReadOperationResult GetImageTextDataFromURL(String url) throws Exception{
 
+    Call post(String data, Callback callback) {
+        RequestBody body = RequestBody.create(String.join("", "{\"url\":\"", data, "\"}"), MEDIA_TYPE_URL);
         Request request = new Request.Builder()
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
                 .url(POST_URLBase)
-                .post(RequestBody.create(String.join("{\"url\":\"" + url + "\"}"),MEDIA_TYPE_URL))
+                .post(body)
                 .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
 
-        Response response = client.newCall(request).execute();
-        try {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+    /**
+     * Processes raw file data from URL.
+     *
+     * @param url URL of the file to be received
+     * @return results as a ReadOperationResult to format later
+     * Note: It is only here for testing purposes, don't mind it.
+     */
 
-            System.out.println(response.headers().toString());
-            System.out.println(response.body().string());
-        }
-        catch(IOException e){}
 
-        String operationId = extractOperationIdFromOpLocation(response.headers().get("operation-location"));
-        ComputerVisionImpl vision = (ComputerVisionImpl) compVisClient.computerVision();
+    Call post(String url, String json, Callback callback) {
+        RequestBody body = RequestBody.create(json, MEDIA_TYPE_URL);
+        Request request = new Request.Builder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
+                .url(url)
+                .post(body)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
 
-        return GetReadResult(vision, operationId);
+    Call get(String url, Callback callback) {
+        Request request = new Request.Builder()
+                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+
+    public void GetImageTextDataFromURL(String url)  {
+
+
+        post(POST_URLBase, String.join("", "{\"url\":\"", url, "\"}"), new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (!response.isSuccessful())
+                    throw new IOException("Unexpected code " + response);
+                String operationId = extractOperationIdFromOpLocation(response.headers().get("operation-location"));
+
+                boolean pollForResult = true;
+                while (pollForResult) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ComputerVisionImpl vision = (ComputerVisionImpl) compVisClient.computerVision();
+
+                    results = vision.getReadResult(UUID.fromString(operationId));
+
+                    OperationStatusCodes status = results.status();
+
+                    if (status == OperationStatusCodes.FAILED || status == OperationStatusCodes.SUCCEEDED) {
+                        pollForResult = false;
+                    }
+                }
+                ExtractText();
+
+//                    get(GET_URLBase+operationId, new Callback(){
+//                        @Override
+//                        public void onResponse(@NonNull Call call2, @NonNull Response response2) throws IOException {
+//                            if(!response2.isSuccessful())
+//                                throw new IOException("Unexpected code "+ response2);
+//
+//
+//                            ComputerVisionImpl vision = (ComputerVisionImpl) compVisClient.computerVision();
+//
+//                            ResponseBody responseBody = response2.body();
+//
+//                            String responseBodyText = responseBody.string();
+//                            System.out.println(responseBodyText);
+//
+//                            ReadOperationResult readResult = vision.getReadResult(UUID.fromString(operationId));
+//
+//                            System.out.println(readResult.analyzeResult().readResults().get(0).lines().get(0).text());
+//
+////                            JsonParser parser = new JsonParser();
+////                            JsonObject obj = new JsonParser().parse(responseBodyText).getAsJsonObject();
+////
+////
+////                            ReadOperationResult readResult = new Gson().fromJson(responseBodyText, ReadOperationResult.class);
+//
+//
+//                            OperationStatusCodes status = readResult.status();
+//
+//                            if (status == OperationStatusCodes.FAILED || status == OperationStatusCodes.SUCCEEDED) {
+//                                pollForResult[0] = false;
+//                            }
+//
+//                        }
+//
+//                        @Override
+//                        public void onFailure(@NonNull Call call2, @NonNull IOException ee) {
+//                            ee.printStackTrace();
+//                            pollForResult[0] = false;
+//                        }
+//                    });
+            }
+
+        });
+
+
+//        Response response = client.newCall(request).execute();
+//        try {
+//            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+//
+//            System.out.println(response.headers().toString());
+//            System.out.println(response.body().string());
+//        }
+//        catch(IOException e){}
+
+//        String operationId = extractOperationIdFromOpLocation(response.headers().get("operation-location"));
+//        ComputerVisionImpl vision = (ComputerVisionImpl) compVisClient.computerVision();
+//
+//        return GetReadResult(vision, operationId);
     }
 
 
@@ -128,15 +252,33 @@ public class OCR_Azure_REST {
         return readResults;
     }
 
-    /**
-     * Reads text from raw file data using the Azure API.
-     * @param file File to be received.
-     *             Supported image formats: JPEG, PNG, BMP, PDF and TIFF.
-     *             For the free tier, only the first 2 pages are processed. File size less than 50MB (4MB for the free tier).
-     * @return String object containing the text inside the image separated by lines and pages.
-     * */
-    public String GetOnlyText(File file) throws Exception{
-        ReadOperationResult readResults = GetImageTextData(file);
+//    /**
+//     * Reads text from raw file data using the Azure API.
+//     * @param file File to be received.
+//     *             Supported image formats: JPEG, PNG, BMP, PDF and TIFF.
+//     *             For the free tier, only the first 2 pages are processed. File size less than 50MB (4MB for the free tier).
+//     * @return String object containing the text inside the image separated by lines and pages.
+//     * */
+//    public String GetOnlyText(File file) throws Exception{
+//        ReadOperationResult readResults = GetImageTextData(file);
+//
+//        // Print read results, page per page
+//        StringBuilder builder = new StringBuilder();
+//        for (ReadResult pageResult : readResults.analyzeResult().readResults()) {
+//            System.out.println();
+//            System.out.println("Printing Read results for page " + pageResult.page());
+//
+//            for (Line line : pageResult.lines()) {
+//                builder.append(line.text());
+//                builder.append("\n");
+//            }
+//
+//            System.out.println(builder.toString());
+//        }
+//        return builder.toString();
+//    }
+
+    public String ExtractTextFromReadOperationResult(ReadOperationResult readResults) {
 
         // Print read results, page per page
         StringBuilder builder = new StringBuilder();
@@ -154,9 +296,58 @@ public class OCR_Azure_REST {
         return builder.toString();
     }
 
+//    public String GetOnlyTextFromUrl(String url) throws Exception{
+//        ReadOperationResult readResults = GetImageTextDataFromURL(url);
+//        // Print read results, page per page
+//        StringBuilder builder = new StringBuilder();
+//
+//        for (ReadResult pageResult : readResults.analyzeResult().readResults()) {
+//            System.out.println();
+//            System.out.println("Printing Read results for page " + pageResult.page());
+//
+//            for (Line line : pageResult.lines()) {
+//                builder.append(line.text());
+//                builder.append("\n");
+//            }
+//
+//            System.out.println(builder.toString());
+//        }
+//        return builder.toString();
+//    }
 
-    public static void main(String[] args) throws Exception{
-        File file = new File("C:\\Users\\matey\\Downloads\\image.jpg");
-        System.out.println(new OCR_Azure_REST().GetOnlyText(file));
+    private void ExtractText() {
+        // Print read results, page per page
+        StringBuilder builder = new StringBuilder();
+
+        for (ReadResult pageResult : results.analyzeResult().readResults()) {
+
+            for (Line line : pageResult.lines()) {
+                builder.append(line.text());
+                builder.append("\n");
+            }
+        }
+        resultsText = builder.toString();
+    }
+
+    public String getResultsText() {
+        return resultsText;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+//        File file = new File("C:\\Users\\matey\\Downloads\\image.jpg");
+//        System.out.println(new OCR_Azure_REST().GetOnlyText(file));
+
+        String url = "https://s3.amazonaws.com/thumbnails.venngage.com/template/112a39f4-2d97-44aa-ae3a-0e95a60abbce.png";
+        OCR_Azure_REST ocrClient = new OCR_Azure_REST();
+
+        ocrClient.GetImageTextDataFromURL(url);
+
+        System.out.println(ocrClient.getResultsText());
+//        System.out.println(res);
     }
 }
--- app/src/main/java/com/example/ocrhotel/SecondFragment.kt
+++ app/src/main/java/com/example/ocrhotel/SecondFragment.kt
@@ -2,11 +2,14 @@ package com.example.ocrhotel
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ocrhotel.databinding.FragmentSecondBinding
+import java.io.File
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -33,17 +36,38 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        imageProvider = ImageProvider(this, activity, this::handleImage)
-        binding.uploadImage.setOnClickListener {
-            imageProvider.useGallery()
-        }
+//        imageProvider = ImageProvider(this, activity, this::handleImage)
+//        binding.uploadImage.setOnClickListener {
+//            imageProvider.useGallery()
+//            handleImage(imageProvider)
+//        }
+//
+//        binding.camera.setOnClickListener {
+//            imageProvider.useCamera()
+//        }
 
-        binding.camera.setOnClickListener {
-            imageProvider.useCamera()
-        }
+        handleImageURL("https://s3.amazonaws.com/thumbnails.venngage.com/template/112a39f4-2d97-44aa-ae3a-0e95a60abbce.png")
+
     }
 
-    private fun handleImage(uri: Uri) {
+    private fun handleImage(uri: String) {
+//        val file = File(uri.path)
+        val ocr = OCR_Azure_REST()
+
+        ocr.GetImageTextDataFromURL(uri)
+
+        val output = ocr.resultsText;
+
+        Log.d("OCR", output)
+        // TODO: handle creation of image
+    }
+
+    private fun handleImageURL(url: String) {
+        val ocr = OCR_Azure_REST()
+        ocr.GetImageTextDataFromURL(url)
+        val output = ocr.resultsText
+
+        Log.d("OCR", output)
         // TODO: handle creation of image
     }
 
