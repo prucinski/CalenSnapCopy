0f869743a8dfea204d6c79f703222360610fc02f
Matey <matey_krastev2@hotmail.com>
2021-12-04 21:02:23 +0200

    2 Tests added.

--- app/src/test/java/com/example/ocrhotel/ExampleUnitTest.kt
+++ app/src/test/java/com/example/ocrhotel/ExampleUnitTest.kt
@@ -1,17 +1,35 @@
 package com.example.ocrhotel
 
+import android.util.Log
 import org.junit.Test
-
 import org.junit.Assert.*
+import java.io.File
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class OCRUnitTest{
+    private val client = OCRAzureREST()
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun ocrWorksWithURL() {
+        val url = "https://i.imgur.com/FEiKUeh.jpg"
+        client.getImageTextDataFromURL(url){
+                it -> it?.let {
+                    assertEquals("09.11",it.contains("09.11"))
+                }
+        }
+    }
+
+    // Needs to have the file be present.
+    @Test
+    fun ocrWorksWithFile() {
+        val file = File("C:\\Users\\matey\\Downloads\\event.jpg")
+        client.getImageTextData(file){
+                it -> it?.let {
+                    assertEquals("09.11",it.contains("09.11"))
+                }
+        }
     }
 }
