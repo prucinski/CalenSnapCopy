dbf2ac28f54fcbea40cf4cf6cf52622193d04aa6
Matey <matey_krastev2@hotmail.com>
2022-02-01 16:50:22 +0000

    Make the date regex more flexible and include regex for time.

--- app/src/main/java/com/example/ocrhotel/Algorithm.kt
+++ app/src/main/java/com/example/ocrhotel/Algorithm.kt
@@ -5,6 +5,8 @@ import java.time.*
 import java.util.*
 import kotlin.math.abs
 
+val testCases = listOf("3 feb 2020", "03 feb 2020", "02-03-2020","3 feb 20","3 feb 2020 10pm","3 feb 2020 10:30","10:00 3 feb 2020");
+
 const val exampleString =
     "Organised by SPE International Aberdeen Section Well Decommissioning - The Future 20 April 2022, P&J Live SUBMIT YOUR ABSTRACT TODAY!"
 
@@ -53,41 +55,66 @@ class Algorithm {
     }
 
     private val monthsString = months.keys.joinToString(separator = "|")
-    private val regex = Regex("""(\d\d)[/ .-]($monthsString|\d?\d)[/ .-]((?:\d\d)?\d\d)""")
 
-    fun extractDates(text: String): Sequence<LocalDateTime> {
+    // Default expression:
+    // Handles everything from DD-MM-YYYY, DD-MMM-YYYY, MM-DD-YYYY, MMM-DD-YYYY
+    // along with short date formats (DD MM) and ordinal suffixes (1st, 2nd, etc.)
+    // Doesn't work with YYYY-MM-DD.
+
+
+    // Solution/refactoring for future: Check the region of the device and determine the setting automatically
+    // or include it as an option in the settings menu.
+    private val regex_date_def = Regex("""($monthsString|\b\d{1,2}\b)[ ./-]?(\d{1,2})(?:st|nd|rd|th)?[ ./-]?(\b\d{2,4}\b)?|(\d{1,2})(?:st|nd|rd|th)?[ ./-]?($monthsString|\b\d{1,2}\b)[ ./-]?(\b\d{2,4}\b)?""")
+
+    // Handles DD MMM, MMM DD with 1st,2nd, etc. (Deprecated for now since default handles everything)
+    // private val regex_date_def_short = Regex("""($monthsString|\b\d{1,2}\b)[ ./-]?(\d{1,2})(?:st|nd|rd|th)?|(\d{1,2})(?:st|nd|rd|th)?[ ./-]?($monthsString|\b\d{1,2}\b)""")
+
+    private val regex_time = Regex("""(\d?\d)[.:,](\d?\d)([am|pm])?""")
+
+    fun extractDates(string: String, regex: Regex=regex_date_def): Sequence<LocalDateTime> {
+
+        val text = string.lowercase()
 
         // Match the OCR text against regex to find all matches
-        val matches = regex.findAll(text.lowercase())
+        val matches = regex.findAll(text)
+
+        val currentDate = LocalDateTime.now()
 
         // Convert matches into LocalDateTime objects
         val dates: Sequence<LocalDateTime> = matches.map { match ->
-            // group at index 1 is day
-            val day = match.groups[1]?.value?.toInt()
 
-            // group at index 2 is month
-            val month = match.groups[2]?.value?.let { m ->
+            val flag = match.groups[4]?.value == null
+
+            // group at index 2/4 is day
+            val day = match.groups[if(flag) 2 else 4]?.value?.toIntOrNull() ?: currentDate.dayOfMonth
+
+            // group at index 1/5 is month
+            val month = match.groups[if(flag) 1 else 5]?.value?.let { m ->
 
                 // check if the month is represented as a number
                 var res = m.toIntOrNull()
                 if (res == null) {
-                    // If the month is not a number, check the months HashMap for the co-responding month value
-                    res = months[m]
+                    // If the month is not a number, check the months HashMap for the corresponding month value
+                    res = months[m+1]
                 }
+                // Handles a really weird mistake in the parsing of a date like '10:00 3 feb 2020'
+                if(res==0) res+=1
                 res
             }
-            val yearStr = match.groups[3]?.value
-            var year = yearStr?.toInt()!! // group at index 3 is year
+
+            val yearStr = match.groups[if(flag) 3 else 6]?.value ?: currentDate.year.toString()
+            println(match.groups)
+            var year = yearStr?.toInt() // group at index 3/6 is year
             if(yearStr.length == 2)  { // its only the 2 last digits of the year
                 year += 2000 // TODO: in 80 years this wont work anymore ;)
             }
 
             if (year == null || month == null || day == null) {
+                return@map currentDate
             } else {
-                // TODO: Handle times
+                // TODO: Handle times (hours and minutes)
                 return@map LocalDateTime.of(year, month, day, 0, 0, 0)
             }
-            return@map LocalDateTime.now()
         }
 
         // Convert the localDateTimes to unix epoch timestamps
@@ -135,6 +162,14 @@ fun main() {
     // Main function that can be used to test functionality outside of android.
     val algorithm = Algorithm()
 
+    // The intended date here is the third of February
+
+    for (test in testCases){
+        println("Start test \"$test\":")
+        for (date in algorithm.extractDates(test)){
+            println(date)
+        }
+    }
 //    for (date in algorithm.extractDates(exampleString)) {
 //        println(Instant.ofEpochSecond(date))
 //    }
@@ -145,10 +180,11 @@ fun main() {
 //        println("\nThis is the final result:"+algorithm.extractTitleFromReadOperationResult(it))
 //    }
 
-    val client = OCRAzureREST()
-    client.getImageTextDataFromURL(url) {
-        println(client.resultsText)
-        val result = algorithm.execute(client.resultsText, client.results);
-        println("\nThis is the final result:" + result.toString())
-    }
+//    val client = OCRAzureREST()
+//    client.getImageTextDataFromURL(url) {
+//        println(client.resultsText)
+//        val result = algorithm.execute(client.resultsText, client.results);
+//        println("\nThis is the final result:" + result.toString())
+//    }
+
 }
