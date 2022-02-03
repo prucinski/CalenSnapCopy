814aa6c38a11f3ce14552d1456ac4d811b350105
erikps <erikps>
2022-02-03 14:32:46 +0000

    Added basics for the python api .

---
+++ database/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_cors import CORS
+import os
+import psycopg2
+
+
+# Flask housekeeping
+app = Flask(__name__, template_folder='static')
+CORS(app)
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+# Error handling for the case of "DATABASE_URL" not being set.
+if (DATABASE_URL is None):
+    app.logger.error(
+        'The environment variable "DATABASE_URL" is not set.')
+
+app.logger.info(f'Trying to connect to database at {DATABASE_URL}')
+
+# Establish a connection to the postgres server using the environment variable "DATABASE_URL"
+connection = psycopg2.connect(DATABASE_URL)
+
+@app.route('/')
+def index():
+    # For now, retrieve the postgres version and send it on the "/" route
+    cursor = connection.cursor()
+    cursor.execute('SELECT version()')
+    version = cursor.fetchone()
+
+    # This is sent as a JSON Object
+    return {'version': version}
+
+if __name__ == '__main__':
+    app.run()
