888bd289c3cefab7e657f780ff6a496e05ecd0b7
erikps <erikps>
2022-02-21 16:44:12 +0000

    Added functions for all used http verbs.

--- app/src/main/java/com/example/ocrhotel/API.kt
+++ app/src/main/java/com/example/ocrhotel/API.kt
@@ -1,35 +1,65 @@
 package com.example.ocrhotel
 
-import com.microsoft.azure.cognitiveservices.vision.computervision.implementation.ComputerVisionImpl
-import com.microsoft.azure.cognitiveservices.vision.computervision.models.OperationStatusCodes
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.File
 import java.io.IOException
-import java.util.*
+
+private val baseUrl = HttpUrl.Builder().scheme("https").host("calensnap-api.herokuapp.com")
+
+private fun path(path: String): HttpUrl {
+    return baseUrl.addPathSegment(path).build()
+}
+
+private fun baseRequest(url: HttpUrl): Request.Builder {
+    return Request.Builder()
+        .addHeader("Content-Type", "application/json")
+        .url(url)
+}
 
 
+private fun makeCall(req: Request, callback: Callback): Call {
+    val call = client.newCall(req)
+    call.enqueue(callback)
+    return call
+}
 
-    private fun callAPI(data: ByteArray, callback: Callback): Call {
-        val request = Request.Builder()
-            .addHeader("Content-Type", "application/json")
-            .url("https://calensnap-api.herokuapp.com")
-            //.post(data.toRequestBody())
-            .build()
-        val call = client.newCall(request)
-        call.enqueue(callback)
-        return call
-    }
 
-    private val client = OkHttpClient()
-    private val MEDIA_TYPE_URL = "application/json".toMediaType()
+private fun get(url: HttpUrl, callback: Callback): Call {
+    val request = baseRequest(url)
+        .build()
+    return makeCall(request, callback)
+}
 
+private fun post(url: HttpUrl, data: ByteArray, callback: Callback): Call {
+    val request = baseRequest(url)
+        .post(data.toRequestBody())
+        .build()
+    return makeCall(request, callback)
+}
+
+private fun put(url: HttpUrl, data: ByteArray, callback: Callback): Call {
+    val request = baseRequest(url)
+        .put(data.toRequestBody())
+        .build()
+    return makeCall(request, callback)
+}
+
+private fun delete(url: HttpUrl, callback: Callback): Call {
+    val request = baseRequest(url)
+        .delete()
+        .build()
+    return makeCall(request, callback)
+}
+
+private val client = OkHttpClient()
+private val MEDIA_TYPE_URL = "application/json".toMediaType()
 
 
 fun main(args: Array<String>) {
 
-    callAPI(ByteArray(0), object : Callback {
+    get(path("/"), object : Callback {
         override fun onFailure(call: Call, e: IOException) {
             e.printStackTrace()
         }
