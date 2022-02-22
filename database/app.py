df50f97b5e0fbe4ba4bb915318d16c49fcb2d39e
erikps <erikps>
2022-02-22 20:55:25 +0000

    Added get_events implementation.

--- database/app.py
+++ database/app.py
@@ -6,7 +6,6 @@ import psycopg2
 import psycopg2.extras
 
 
-
 # Flask housekeeping
 app = Flask(__name__, template_folder='static')
 CORS(app)
@@ -27,6 +26,7 @@ if (DATABASE_URL is None):
 def connect():
     """ Returns a connection to the database. """
     return psycopg2.connect(DATABASE_URL)
+
 
 @app.route('/')
 def index():
@@ -52,6 +52,7 @@ def create_profile():
 
     return 'not implemented', 501
 
+
 @app.route('/profile/<uuid:profile_id>', methods=['DELETE'])
 def delete_profile(profile_id):
     """ Delete the specified profile. """
@@ -63,9 +64,18 @@ def delete_profile(profile_id):
 @app.route('/events/<uuid:profile_id>', methods=['GET'])
 def get_events(profile_id):
     """ Retrieve all events that were created by the user with 'profile_id'. """
-    # TODO: implement
-    return 'not implemented', 501
 
+    try:
+        connection = connect()
+        cursor = connection.cursor()
+
+        cursor.execute(""" SELECT * FROM event; """)
+        events = cursor.fetchall()
+
+        return {'events': map(lambda x: {'event_id': x[0], 'event_time': x[1], 'event_location': x[2]}, events)}, 200
+
+    except:
+        return {'success': False}, 400
 
 
 @app.route('/events/<uuid:profile_id>', methods=['POST'])
@@ -75,12 +85,13 @@ def create_event(profile_id):
     try:
         connection = connect()
         cursor = connection.cursor()
-        
+
         # Find out if the profile id exists
-        cursor.execute("""SELECT id FROM profile WHERE id = %s;""", (profile_id,))
-        
+        cursor.execute(
+            """SELECT id FROM profile WHERE id = %s;""", (profile_id,))
+
         # This will fail if there is no profile with that id, which means the try block will exit and the except block will be executed
-        cursor.fetchone() 
+        cursor.fetchone()
 
         # Extract JSON data from HTTP request to put it into the database.
         event_time = request.json['event_time']
@@ -92,16 +103,16 @@ def create_event(profile_id):
                VALUES (%s, POINT(%s, %s)) RETURNING id;
             """,
             (event_time, event_location['N'], event_location['W']))
-        
+
         (event_id, ) = cursor.fetchone()
-        connection.commit() # write changes
+        connection.commit()  # write changes
         app.logger.warning(event_id)
         return {'event_id': event_id}, 200
 
     except Exception as e:
         app.logger.warning("Error: ", e)
         # The request did not contain the appropriate json
-        return {'success':False}, 400
+        return {'success': False}, 400
 
 
 @app.route('/events/<uuid:event_id>', methods=['DELETE'])
@@ -109,7 +120,6 @@ def delete_event(event_id):
     """ Delete the specified event. """
     # TODO: implement
     return 'not implemented', 501
-
 
     # This is for locally testing the application
 if __name__ == '__main__':
