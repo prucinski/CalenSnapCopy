bc3b45706137b95eb51352073da1305ef17f50d2
Stanislav Stoyanov <stani30000@gmail.com>
2022-02-22 23:34:52 +0000

    unit tests create

--- app/src/test/java/com/example/ocrhotel/Test_OCRAzureREST.kt
+++ app/src/test/java/com/example/ocrhotel/Test_OCRAzureREST.kt
@@ -5,6 +5,7 @@ import org.junit.Test
 import org.junit.Assert.*
 import java.io.File
 import java.lang.Exception
+import java.time.LocalDateTime
 import java.util.*
 
 
@@ -15,7 +16,8 @@ import java.util.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class OCRUnitTest{
-
+    val algorithm = Algorithm()
+    var currenttime = ""
     @Test
     fun ocrWorksWithURL() {
         val client = OCRAzureREST()
@@ -82,8 +84,88 @@ class OCRUnitTest{
         assertEquals(false, stringToCheck.contains("unacceptable nonsense"))
 
     }
+
     @Test
     fun testFunction(){
         assertEquals(2, 2)
+    }
+
+    @Test
+    fun test1(){
+        assertEquals("2020-02-03T12:00",algorithm.extractDates("03 02 2020").elementAt(0).toString())
+    }
+    @Test
+    fun test2(){
+        assertEquals("2020-02-03T12:00",algorithm.extractDates("03 feb 2020").elementAt(0).toString())
+    }
+    @Test
+    fun test3(){
+        assertEquals("2020-02-29T12:02",algorithm.extractDates("29.02.2020").elementAt(0).toString())
+        // time 12:02?
+    }
+    @Test
+    fun test4(){
+        currenttime = LocalDateTime.now().toString()
+        assertEquals(currenttime.dropLast(13),algorithm.extractDates("29.02.2021").elementAt(0).toString().dropLast(13)) // returns current date and time
+    }
+    @Test
+    fun test5(){
+        assertEquals("2000-02-29T12:02",algorithm.extractDates("29.02.2000").elementAt(0).toString())
+    }
+
+    @Test
+    fun test6(){
+        currenttime = LocalDateTime.now().toString()
+        assertEquals(currenttime.dropLast(13),algorithm.extractDates("30.02.2021").elementAt(0).toString().dropLast(13)) // returns current date and time
+    }
+
+    @Test
+    fun test7() {
+        currenttime = LocalDateTime.now().toString()
+        assertEquals(currenttime.dropLast(13),algorithm.extractDates("30.02.2020").elementAt(0).toString().dropLast(13) // returns current date and time
+        )
+    }
+    @Test
+    fun test8(){
+        currenttime = LocalDateTime.now().toString()
+        assertEquals(currenttime.dropLast(13),algorithm.extractDates("30.02.2000").elementAt(0).toString().dropLast(13)) // returns current date and time
+    }
+    @Test
+    fun test9(){
+        currenttime = LocalDateTime.now().toString()
+        assertEquals(currenttime.dropLast(13),algorithm.extractDates("31.06.2020").elementAt(0).toString().dropLast(13)) // returns current date and time
+    }
+    @Test
+    fun test10(){
+        currenttime = LocalDateTime.now().toString()
+        assertEquals(currenttime.dropLast(13),algorithm.extractDates("31.06.2020").elementAt(0).toString().dropLast(13)) // returns current date and time
+    }
+    @Test
+    fun test11(){
+        assertEquals("2020-01-22T00:01",algorithm.extractDates("00.01.2020").elementAt(0).toString())
+    }
+    @Test
+    fun test12(){
+        assertEquals("2004-02-22T22:04",algorithm.extractDates("20-22.04.2020").elementAt(0).toString())
+    }
+    @Test
+    fun test13(){
+        assertEquals("2020-04-22T22:04",algorithm.extractDates("22.04.2020 at 20:20").elementAt(0).toString())
+    }
+    @Test
+    fun test14(){
+        assertEquals("2020-04-22T22:04",algorithm.extractDates("22.04.2020 at 20:30").elementAt(0).toString())
+    }
+    @Test
+    fun test15(){
+        assertEquals("2020-04-22T22:04",algorithm.extractDates("22.04.2020 at 2030").elementAt(0).toString())
+    }
+    @Test
+    fun test16(){
+        assertEquals("2020-04-22T22:04",algorithm.extractDates("22.04.2020 at 2020").elementAt(0).toString())
+    }
+    @Test
+    fun test17(){
+        assertEquals("2020-04-20T20:04",algorithm.extractDates("20.04.2020 at 18pm").elementAt(0).toString())
     }
 }
