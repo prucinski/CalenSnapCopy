fa0f2ebb3e5f1f3a6daf089d50a98026432469f7
Matey <matey_krastev2@hotmail.com>
2022-03-03 17:02:53 +0000

    Bottom navigation bar implemented.

--- app/src/main/java/com/example/ocrhotel/MainActivity.kt
+++ app/src/main/java/com/example/ocrhotel/MainActivity.kt
@@ -9,10 +9,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.ocrhotel.databinding.ActivityMainBinding
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,23 +30,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         
         //Code for the banner ads
-
         MobileAds.initialize(this) {}
 
         val mAdView = findViewById<AdView>(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
 
-
+        val firstFragment = MainMenu()
+        val secondFragment = SecondFragment()
 
         binding.fab.setOnClickListener {
-            //Navigate from Floating Action Button to Settings menu. Can be clicked multiple
-            //times and we don't really want that. T
-            //TODO: Check if the fragment you're in is settings, if yes don't navigate
-            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            // Go to scanning
+            setCurrentFragment(secondFragment)
+        }
 
-                navController.navigate(R.id.action_global_settingsMenu)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                // Go to home (currently just the previously declared SCAN+tutorial screen)
+                R.id.navigation_home -> setCurrentFragment(firstFragment)
 
+                // Go to tutorial / help page
+                R.id.navigation_help -> Toast.makeText(baseContext, "This will lead to a tutorial!", Toast.LENGTH_SHORT).show()
+
+                // Go to the history page
+                R.id.navigation_history -> Toast.makeText(baseContext , "This will to the history!", Toast.LENGTH_SHORT).show()
+
+                // Go to the settings page
+                R.id.navigation_settings -> Toast.makeText(baseContext, "This will lead to the settings menu!", Toast.LENGTH_SHORT).show()
+
+            }
+            return@setOnItemSelectedListener true
+        }
+
+
+
+        // binding.fab.setOnClickListener {
+        //     // Navigate form
+        //     val navController = findNavController(R.id.nav_host_fragment_content_main)
+        //
+        //     navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //
+        // }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_content,fragment)
+            commit()
         }
     }
 
---
+++ app/src/main/java/com/example/ocrhotel/Navigation.kt
@@ -0,0 +1,4 @@
+package com.example.ocrhotel
+
+class Navigation {
+}
