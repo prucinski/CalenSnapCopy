93e9bd96123a2dd67e46db987b2eb70b3eb5886e
Matey <matey_krastev2@hotmail.com>
2022-03-10 17:36:59 +0000

    ModifyEvent.kt cleaned and calendar working.

--- app/src/main/java/com/example/ocrhotel/ModifyEvent.kt
+++ app/src/main/java/com/example/ocrhotel/ModifyEvent.kt
@@ -2,38 +2,41 @@ package com.example.ocrhotel
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import android.widget.AdapterView
-import android.widget.ArrayAdapter;
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
-import androidx.fragment.app.setFragmentResultListener
 import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
 import com.example.ocrhotel.databinding.FragmentModifyEventBinding
-import com.example.ocrhotel.databinding.FragmentSecondBinding
-import com.microsoft.azure.cognitiveservices.vision.computervision.models.ReadOperationResult
-import com.microsoft.azure.cognitiveservices.vision.computervision.models.ReadOptionalParameter
+import com.google.android.material.datepicker.MaterialDatePicker
+import org.joda.time.LocalTime
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 class ModifyEvent : Fragment(R.layout.fragment_modify_event) {
 
 
-    //This will decide how many entries will be generated with the spinner.
-    private var numberOfEvents = 3
-    //The events list. It is modifiable.
-    private var eventsList: List<Event> = emptyList()
-    //Keep track which event we're looking at now. By default we're looking at the first event.
-    private var currentEvent = 0;
 
+    // The events list. It is modifiable.
+    private var eventsList: List<Event> = emptyList()
+
+    // Keep track which event we're looking at now. By default we're looking at the first event.
+    private var currentEvent = 0
+
+    // This will decide how many entries will be generated with the spinner.
+    private var numberOfEvents = 0
+
+    // Custom date format that we may use while displaying the date to clients.
+    private val dateFormatter : DateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-uuuu")
 
     private var _binding: FragmentModifyEventBinding? = null
     // This property is only valid between onCreateView and
@@ -64,18 +67,38 @@ class ModifyEvent : Fragment(R.layout.fragment_modify_event) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        //TODO: GET THAT EVENT ARRAY - change key
-        //receive the event list from SecondFragment.
+        // TODO: GET THAT EVENT ARRAY - change key
+        // Receive the event list from SecondFragment.
         eventsList = arguments?.getSerializable("data") as List<Event>
         numberOfEvents = eventsList.size
-        Log.e("List size", " in ModifyEvent on creation: $numberOfEvents")
-        val fillEvents = getResources().getStringArray(R.array.events)
+        Log.d("List size", " in ModifyEvent on creation: $numberOfEvents")
+
+        val fillEvents = resources.getStringArray(R.array.events)
         super.onViewCreated(view, savedInstanceState)
+
         //Applying the spinner
         val spinner: Spinner = binding.foundEventsSelector
-        ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, fillEvents.slice(0..numberOfEvents-1))
+        ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, fillEvents.slice(0 until numberOfEvents))
             .also{adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter}
+
+        binding.EventDate.setOnClickListener{
+            val currentDate = LocalDate.parse(binding.EventDate.text.toString(), dateFormatter)
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Select date")
+                    .setTheme(R.style.ThemeOverlay_App_MaterialCalendar)
+                    .setSelection(currentDate.atStartOfDay().toEpochSecond(ZoneOffset.UTC)*1000)
+                    .build()
+            datePicker.show(parentFragmentManager,"")
+
+            datePicker.addOnPositiveButtonClickListener {
+                binding.EventDate.text = LocalDateTime.ofEpochSecond(
+                    datePicker.selection!!/1000,0, ZoneOffset.UTC)
+                    .format(dateFormatter)
+            }
+
+        }
 
 
         //button "Continue".
@@ -84,7 +107,7 @@ class ModifyEvent : Fragment(R.layout.fragment_modify_event) {
                 //request permission from user to access their calendars
                 ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR), 1)
                 if(checkIfHasPermission()) {
-                    var eventCreator = EventCreator(eventsList, activity)
+                    val eventCreator = EventCreator(eventsList, activity)
                     //this is slightly wonky and has to be pressed twice - but it's a minor bug
                     if (eventCreator.addEvent()) {
                         findNavController().navigate(R.id.action_modifyEvent_to_successfulScan)
@@ -102,7 +125,7 @@ class ModifyEvent : Fragment(R.layout.fragment_modify_event) {
                 // An item was selected. You can retrieve the selected item using
                 Toast.makeText(context, "This has been selected $pos", Toast.LENGTH_SHORT).show()
                 currentEvent = pos
-                binding.EventDate.setText(eventsList[currentEvent].eventDate)
+                binding.EventDate.text = eventsList[currentEvent].eventDate
                 binding.EventHour.setText(eventsList[currentEvent].eventHour)
                 binding.eventTitle.setText(eventsList[currentEvent].eventName)
             }
@@ -112,39 +135,32 @@ class ModifyEvent : Fragment(R.layout.fragment_modify_event) {
             }
         }
 
-        //Button "submit".
+        //Button "Submit".
         binding.submit.setOnClickListener {
-            //once the button is pressed, modify the values inside the list.
+            // Once the button is pressed, modify the values inside the list.
             eventsList[currentEvent].eventName = binding.eventTitle.text.toString()
-            //spaghetti
-            //TODO - throw this out and fix UI
-            val year = binding.EventDate.text.toString().slice(6..9).toInt()
-            val month = binding.EventDate.text.toString().slice(3..4).toInt()
-            val day = binding.EventDate.text.toString().slice(0..1).toInt()
-            val hr = binding.EventHour.text.toString().slice(0..1).toInt()
-            val min = binding.EventHour.text.toString().slice(3..4).toInt()
-            eventsList[currentEvent].eventDateTime = LocalDateTime.of(year, month, day, hr, min)
-            //I'm not sure if the next two lines are necessary, but I think they are since
-            //the object has already been constructed.
+
+            val date: LocalDate = LocalDate.parse(binding.EventDate.text, dateFormatter)
+            val time: LocalTime = LocalTime.parse(binding.EventHour.text.toString())
+            eventsList[currentEvent].eventDateTime = LocalDateTime.of(
+                date.year, date.monthValue, date.dayOfMonth, time.hourOfDay, time.minuteOfHour)
+
+            // I'm not sure if the next two lines are necessary, but I think they are since
+            // the object has already been constructed.
             eventsList[currentEvent].eventDate = binding.EventDate.text.toString()
             eventsList[currentEvent].eventHour = binding.EventHour.text.toString()
-            //printEventDetails.show()
+
             //TODO: Automatically move user to next event. Fairly simple but left  it for now
         }
     }
     private fun checkIfHasPermission() :Boolean{
-        var result = context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.READ_CALENDAR) }
+        val result = context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.READ_CALENDAR) }
         Log.e("PERMISSION", "$result" )
         return result == PackageManager.PERMISSION_GRANTED
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
-
-
 }
