8774157a850118dddeed69ccf0e15d8ca7df4626
Matey <matey_krastev2@hotmail.com>
2022-03-17 22:22:54 +0000

    Event History mostly implemented.

--- app/src/main/java/com/example/ocrhotel/EventsHistoryFragment.kt
+++ app/src/main/java/com/example/ocrhotel/EventsHistoryFragment.kt
@@ -1,13 +1,14 @@
 package com.example.ocrhotel
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ocrhotel.placeholder.PlaceholderContent
 
 
@@ -38,6 +39,12 @@ class EventsHistoryFragment : Fragment() {
                     else -> GridLayoutManager(context, columnCount)
                 }
                 adapter = MyEventsHistoryRecyclerViewAdapter(PlaceholderContent.ITEMS)
+                // val dividerItemDecoration = DividerItemDecoration(
+                //     this.context,
+                //     1
+                // )
+                // this.addItemDecoration(dividerItemDecoration)
+
             }
         }
         return view
