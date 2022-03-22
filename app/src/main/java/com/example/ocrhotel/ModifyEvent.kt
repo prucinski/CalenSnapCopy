0e1cf613934680afbe6ee04e6e858138caed4c17
Alexandra <a.s.nenkova@gmail.com>
2022-03-22 15:03:26 +0000

    Tried to update the visual aspect of the app. Needs work

--- app/src/main/java/com/example/ocrhotel/ModifyEvent.kt
+++ app/src/main/java/com/example/ocrhotel/ModifyEvent.kt
@@ -49,7 +49,7 @@ class ModifyEvent : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    
+
     private val adRequest = AdRequest.Builder().build()
     private var mInterstitialAd: InterstitialAd? = null
     private var TAG = "Interstitial Ad"
@@ -101,7 +101,6 @@ class ModifyEvent : Fragment() {
             val datePicker =
                 MaterialDatePicker.Builder.datePicker()
                     .setTitleText("Select date")
-                    .setTheme(R.style.ThemeOverlay_App_MaterialCalendar)
                     .setSelection(currentDate.atStartOfDay().toEpochSecond(ZoneOffset.UTC)*1000)
                     .build()
 
