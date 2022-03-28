a097a414c35fcb96cf54678e7141a1e39e0b2d90
prucinski <p.rucinski.19@abdn.ac.uk>
2022-03-28 18:47:00 +0100

    added google pay resources. Implementing payment system now

---
+++ app/src/main/java/com/example/ocrhotel/PaymentPopup.kt
@@ -0,0 +1,18 @@
+package com.example.ocrhotel
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+
+class PaymentPopup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        overridePendingTransition(0, 0)
+        setContentView(R.layout.activity_payment_popup)
+
+    }
+}
