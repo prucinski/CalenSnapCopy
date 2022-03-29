d470c19c7d2e882932e96e58ed6d81139c0400f8
erikps <43610516+erikps@users.noreply.github.com>
2022-03-29 18:18:22 +0100

    Now updates shared preferences with the JWT.

--- app/src/main/java/com/example/ocrhotel/LoginFragment.kt
+++ app/src/main/java/com/example/ocrhotel/LoginFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.ocrhotel.databinding.FragmentLoginBinding
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginFragment : Fragment() {
 
@@ -33,11 +34,34 @@ class LoginFragment : Fragment() {
         val username = binding.usernameInput.text.toString()
         val password = binding.passwordInput.text.toString()
 
+        if (username.isBlank() || password.isBlank()) {
+            Toast.makeText(
+                requireContext(),
+                "Please provide username and password.",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
         login(username, password) { jwt ->
             if (jwt != null) {
                 Log.w("JWT SUCCESS: ", jwt)
                 activity?.runOnUiThread {
                     Toast.makeText(requireContext(), "Login successful!", Toast.LENGTH_SHORT).show()
+
+
+                    val sh = activity?.getSharedPreferences(
+                        getString(R.string.preferences_address),
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    if (sh != null) {
+                        // Store JWT as a shared preference
+                        val edit = sh.edit()
+                        edit.putString("JWT", jwt)
+                        edit.apply()
+                    }
+
                 }
             } else {
                 Log.w("JWT FAILURE: ", jwt)
@@ -48,7 +72,6 @@ class LoginFragment : Fragment() {
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-
             }
         }
     }
