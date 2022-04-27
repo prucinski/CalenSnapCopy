b164168d35dc375e907291422980f9796967fb18
Leah Hughes <60482637+lkhughes@users.noreply.github.com>
2022-04-27 16:55:04 +0100

    Fixed mistake with event time
    Was saving event time into both event and userevent, fixed it so now it's saving event time to userevent and snaptime to event as it should be

--- database/app.py
+++ database/app.py
@@ -230,7 +230,7 @@ def create_event():
             """ INSERT INTO event (snap_time, snap_location)
                 VALUES (%s, POINT(%s, %s)) RETURNING id;
             """,
-            (event_time, snap_location['N'], snap_location['W']))
+            (snap_time, snap_location['N'], snap_location['W']))
         (event_id, ) = cursor.fetchone()
 
         cursor = connection.cursor()
